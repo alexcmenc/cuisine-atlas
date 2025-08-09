1b58acd51b53167cf05f291a79284699619e8205
Alec E <alexcmenc@gmail.com>
2025-08-09 11:01:51 +0100

    fix CORS2

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173", // dev
-  "http://cuisineatlas.netlify.app",
+  "https://cuisineatlas.netlify.app/",
 ];
 
 app.use(
